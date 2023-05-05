e8b5b2367b47c91a71708ad9f5a43627be63f615
naveengitboi <naveenjangiti9@gmail.com>
2023-05-05 08:01:01 +0530

    added hamburger menu

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,11 +1,25 @@
 import { NavLink } from "react-router-dom";
 import '../cssComponents/Navbar.css'
+import { useState } from "react";
 function Navbar(){
+    const [hamburger, setHamburger] = useState('hamburger')
+    const [active, setActive] = useState(false)
+    const [navlinks, setNavlinks] = useState('navLinks')
+
+    function handleMenu(){
+        if(!active){
+            setHamburger(prev => prev = 'hamburgerActive')
+            setNavlinks(prev => prev = 'navLinksActive')
+        }
+
+    }
+
+
     return(
         <div className="navbar">
             <h1 className="mediumHeading">Naveen</h1>
-            <div className="hamburger"></div>
-            <ul className="navLinks">
+            <div className={hamburger} onClick={()=> handleMenu()}></div>
+            <ul className={navlinks}>
                 <li><NavLink to={'/'}>Home</NavLink></li>
                 <li><NavLink to={'/gallary'}>Gallary</NavLink></li>
                 <li><NavLink to={'/contact'}>Contact</NavLink></li>
